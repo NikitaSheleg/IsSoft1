39cc7b00b02feaa86015ffa833597f586d27a4ad
ShteinRose <enotpoloskun08062001@gmail.com>
2021-03-25 00:11:49 +0300

    modules added

---
+++ binarySearchModule/src/main/java/BinarySearch.java
@@ -0,0 +1,26 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BinarySearch {
+    private static final Logger logger = LoggerFactory.getLogger(BinarySearch.class);
+    private static final Logger secondLogger = LoggerFactory.getLogger("logger");
+
+    public static int binarySearchMethod(int[] array, int start, int end, int target) {
+
+        int middleElementIndex = (end + start) / 2;
+        logger.debug("middle {}", middleElementIndex);
+        secondLogger.debug("middle {}", middleElementIndex);
+        if (end < start) {
+            throw new IllegalArgumentException("end<start");
+        }
+
+        if (target == array[middleElementIndex]) {
+            return middleElementIndex;
+        } else if (target < array[middleElementIndex]) {
+            return binarySearchMethod(array, start, middleElementIndex - 1, target);
+        } else return binarySearchMethod(array, middleElementIndex + 1, end, target);
+    }
+
+
+
+}
---
+++ mainModule/src/main/java/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = new int[5];
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        array[3] = 4;
+        array[4] = 5;
+        BinarySearch.binarySearchMethod(array, 0, array.length - 1, 1);
+
+    }
+}

58931dcfa246c9ba0b144b3506f08d89bf6c6525
ShteinRose <enotpoloskun08062001@gmail.com>
2021-03-25 12:56:43 +0300

    reduant files and logger removed

--- binarySearchModule/src/main/java/BinarySearch.java
+++ binarySearchModule/src/main/java/BinarySearch.java
@@ -2,17 +2,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class BinarySearch {
-    private static final Logger logger = LoggerFactory.getLogger(BinarySearch.class);
     private static final Logger secondLogger = LoggerFactory.getLogger("logger");
 
     public static int binarySearchMethod(int[] array, int start, int end, int target) {
-
 
         if (end < start) {
             throw new IllegalArgumentException("end<start");
         }
         int middleElementIndex = (end + start) / 2;
-        logger.debug("middle {}", middleElementIndex);
         secondLogger.debug("middle {}", middleElementIndex);
 
         if (target == array[middleElementIndex]) {
